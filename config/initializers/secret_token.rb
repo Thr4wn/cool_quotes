171c24b8d62d1a6ea084824e145ea39feff2d3b6
Alexander Bird <alexander.bird.42@gmail.com>
2016-05-22 22:57:53 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DevopsPocApp::Application.config.secret_token = '7d516d0fbbd6572d91c4a22b4491d402e2fb6b1ccd70a2cb2d3a61536c23aa962d220b4cbdd5191933b3a0d898e291c6578d1aef4b3301ea3fa8208dc4c83426'
