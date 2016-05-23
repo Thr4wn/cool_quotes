c4e77ab44184b4dbde288d347f1d8a0c81f1c526
Alexander Bird <alexander.bird.42@gmail.com>
2016-05-23 02:09:46 +0000

    first attempt to actually have rails server running

--- cool_quotes-webserver/recipes/default.rb
+++ cool_quotes-webserver/recipes/default.rb
@@ -7,13 +7,36 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe "nginx::default"
+#include_recipe "nginx::default"
+#
 
 package 'git'
 
-git "/srv" do
+directory "/usr/local/src/cool_quotes"
+
+git "/usr/local/src" do
     repository 'https://github.com/Thr4wn/cool_quotes.git'
     reference "master"
     action :sync
+end
+
+remote_file "Copy rails app to /srv" do 
+  path "/srv/cool_quotes_srv" 
+  source "file:///usr/local/src/cool_quotes/rails_app"
+end
+
+application '/srv/cool_quotes_srv' do
+  bundle_install do
+    deployment true
+    without %w{development test}
+  end
+  rails do
+    database 'sqlite3:///db.sqlite3'
+    #secret_token 'd78fe08df56c9'
+    migrate true
+  end
+  unicorn do
+    port 8000
+  end
 end
 
